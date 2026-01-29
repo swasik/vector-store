3007032e8dc7a8b8d870d60b0e3dc3d3f3791d60
Pawel Pery <pawel.pery@scylladb.com>
2026-01-29 14:35:16 +0100

    validator: fix full_scan::full_scan_is_completed_when_responding_to_messages_concurrently
    The root cause of this test's flakiness was faster than expected full scan
    process and slower discovering of a new index by vector-store nodes. For this
    reason the intermediate state of vector-store status couldn't be reliably
    observed.
    
    This commit uses single vector-store (we don't observe communication between
    vector-stores), scylla-proxy (to slow communication between vector-store and
    scylladb, to observe intermediate vector-store status).
    
    The commit adds some logging information to easier triage future failures.
    
    Fixes: VECTOR-440

--- crates/validator-vector-store/src/full_scan.rs
+++ crates/validator-vector-store/src/full_scan.rs
@@ -4,6 +4,10 @@
  */
 
 use async_backtrace::framed;
+use scylla_proxy::Condition;
+use scylla_proxy::Reaction;
+use scylla_proxy::RequestReaction;
+use scylla_proxy::RequestRule;
 use std::time::Duration;
 use tracing::info;
 use vector_search_validator_tests::common::*;
@@ -13,7 +17,7 @@ use vector_search_validator_tests::*;
 pub(crate) async fn new() -> TestCase {
     let timeout = DEFAULT_TEST_TIMEOUT;
     TestCase::empty()
-        .with_init(timeout, init)
+        .with_init(timeout, init_with_proxy_single_vs)
         .with_cleanup(timeout, cleanup)
         .with_test(
             "full_scan_is_completed_when_responding_to_messages_concurrently",
@@ -26,7 +30,8 @@ pub(crate) async fn new() -> TestCase {
 async fn full_scan_is_completed_when_responding_to_messages_concurrently(actors: TestActors) {
     info!("started");
 
-    let (session, clients) = prepare_connection(&actors).await;
+    let (session, clients) = prepare_connection_single_vs(&actors).await;
+    let client = clients.first().unwrap();
 
     let keyspace = create_keyspace(&session).await;
     let table = create_table(
@@ -36,8 +41,10 @@ async fn full_scan_is_completed_when_responding_to_messages_concurrently(actors:
     )
     .await;
 
+    info!("Inserting data to the table");
+    const DATASET_SIZE: i32 = 100;
     let embedding: Vec<f32> = vec![0.0, 0.0, 0.0];
-    for i in 0..1000 {
+    for i in 0..DATASET_SIZE {
         session
             .query_unpaged(
                 format!("INSERT INTO {table} (id, embedding) VALUES (?, ?)"),
@@ -47,28 +54,42 @@ async fn full_scan_is_completed_when_responding_to_messages_concurrently(actors:
             .expect("failed to insert data");
     }
 
+    info!("Slow communication between vector-store and scylla using proxy");
+    const FRAME_DELAY: Duration = Duration::from_millis(100);
+    actors
+        .db_proxy
+        .change_request_rules(Some(vec![RequestRule(
+            Condition::True,
+            RequestReaction::delay(FRAME_DELAY),
+        )]))
+        .await;
+
+    info!("Creating index");
     let index = create_index(&session, &clients, &table, "embedding").await;
 
+    info!("Checking that full scan isn't completed");
     let result = session
         .query_unpaged(
             format!("SELECT * FROM {table} ORDER BY embedding ANN OF [1.0, 2.0, 3.0] LIMIT 5"),
             (),
         )
         .await;
-
     match &result {
         Err(e) if format!("{e:?}").contains("503 Service Unavailable") => {}
         _ => panic!("Expected SERVICE_UNAVAILABLE error, got: {result:?}"),
     }
 
-    for client in &clients {
-        let index_status = wait_for_index(client, &index).await;
-        assert_eq!(
-            index_status.count, 1000,
-            "Expected 1000 vectors to be indexed"
-        );
-    }
+    info!("Recovering normal communication");
+    actors.db_proxy.turn_off_rules().await;
 
+    info!("Waiting for index to be built");
+    let index_status = wait_for_index(client, &index).await;
+    assert_eq!(
+        index_status.count, DATASET_SIZE as usize,
+        "Expected {DATASET_SIZE} vectors to be indexed"
+    );
+
+    info!("Checking that ANN search works after index is built");
     session
         .query_unpaged(
             format!("SELECT * FROM {table} ORDER BY embedding ANN OF [1.0, 2.0, 3.0] LIMIT 5"),
@@ -77,20 +98,20 @@ async fn full_scan_is_completed_when_responding_to_messages_concurrently(actors:
         .await
         .expect("failed to query ANN search");
 
+    info!("Dropping index");
     session
         .query_unpaged(format!("DROP INDEX {}", index.index), ())
         .await
         .expect("failed to drop an index");
 
-    for client in &clients {
-        wait_for(
-            || async { client.indexes().await.is_empty() },
-            "Waiting for index deletion",
-            Duration::from_secs(60),
-        )
-        .await;
-    }
+    wait_for(
+        || async { client.indexes().await.is_empty() },
+        "index must be removed",
+        Duration::from_secs(60),
+    )
+    .await;
 
+    info!("Dropping keyspace");
     session
         .query_unpaged(format!("DROP KEYSPACE {keyspace}"), ())
         .await
